58e4a1fad7e46fbd741b140e30036c36599eb759
Juan.lb <ljuan7397@gmail.com>
2023-02-26 20:34:38 +0100

    Complete DNI value object

--- lib/common/src/domain/valueObject/valueObjects/DNI.valueObject.ts
+++ lib/common/src/domain/valueObject/valueObjects/DNI.valueObject.ts
@@ -12,25 +12,43 @@ export class DNI extends ValueObject<string> {
     return this.props.value;
   }
 
-  isLetterValid(DNINumber: number, DNILetter: string): boolean {
+  private DNINumber(validDNI: string): number {
+    return Number(validDNI.substring(0, 8));
+  }
+
+  private DNILetter(validDNI: string): string {
+    return validDNI.charAt(8);
+  }
+
+  public static isLetterValid(DNINumber: number, DNILetter: string): boolean {
     return DNILettersByPosition[DNINumber % 23] === DNILetter;
   }
 
-  isLastCaracter(DNI: string): boolean {
+  public static isLastDigitCharacter(DNI: string): boolean {
     return isString(DNI.charAt(8));
   }
 
-  isFirstEightNumbers(DNI: string): boolean {
-    [...DNI].forEach((DNIElement) => {
+  public static isFirstEightDigitsNumbers(DNI: string): boolean {
+    const DNINumber = DNI.substring(0, 8);
+    [...DNINumber].forEach((DNIElement) => {
       if (!isNumber(DNIElement)) return false;
     });
     return true;
   }
 
-  protected validate({ value: DNI }: DomainPrimitive<string>): void {
-    if (this.isFirstEightNumbers(DNI) && this.isLastCaracter(DNI)) {
-      throw new Error(`DNI "${DNI}" has incorrect format`);
-    }
+  protected validate({ value: DNIValue }: DomainPrimitive<string>): void {
+    const correctFormatDNI =
+      DNI.isFirstEightDigitsNumbers(DNIValue) &&
+      DNI.isLastDigitCharacter(DNIValue);
+
+    if (!correctFormatDNI)
+      throw new Error(`DNI "${DNIValue}" has incorrect format`);
+
+    const validDNI = DNI.isLetterValid(
+      this.DNINumber(DNIValue),
+      this.DNILetter(DNIValue),
+    );
+    if (!validDNI) throw new Error(`DNI ${DNIValue} is incorrect`);
   }
 }
 
